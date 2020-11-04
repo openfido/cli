ac454f0057ec9289f4826f2e7fae7294d2812fd4
David P. Chassin <dchassin@slac.stanford.edu>
2020-11-04 08:23:20 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+curl -sL https://raw.githubusercontent.com/openfido/cli/main/src/openfido > /usr/local/bin/openfido
+curl -sL https://raw.githubusercontent.com/openfido/cli/main/src/openfido.py > /usr/local/bin/openfido.py
+chmod a+x /usr/local/bin/openfido
