2a129af50431f5079c4173745a6f82482863d1f3
David P. Chassin <dchassin@slac.stanford.edu>
2021-02-19 07:57:58 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 curl -sL https://raw.githubusercontent.com/openfido/cli/main/src/openfido > /usr/local/bin/openfido
 curl -sL https://raw.githubusercontent.com/openfido/cli/main/src/openfido.py > /usr/local/bin/openfido.py
+curl -sL https://raw.githubusercontent.com/openfido/cli/main/src/openfido_util.py > /usr/local/bin/openfido_util.py
 chmod a+x /usr/local/bin/openfido
