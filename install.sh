fe127d628a6c63935a9bc78826d7db7e8c303ae0
David P. Chassin <dchassin@slac.stanford.edu>
2022-01-21 08:51:19 -0800

    Allow use of OPENFIDO_BRANCH in install scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-curl -sL https://raw.githubusercontent.com/openfido/cli/main/src/openfido > /usr/local/bin/openfido ; chmod +x /usr/local/bin/openfido
-curl -sL https://raw.githubusercontent.com/openfido/cli/main/src/openfido-server > /usr/local/bin/openfido-server ; chmod +x /usr/local/bin/openfido-server
-curl -sL https://raw.githubusercontent.com/openfido/cli/main/src/openfido.py > /usr/local/bin/openfido.py ; chmod +x /usr/local/bin/openfido.py
-curl -sL https://raw.githubusercontent.com/openfido/cli/main/src/openfido_util.py > /usr/local/bin/openfido_util.py ; chmod +x /usr/local/bin/openfido_util.py
+curl -sL https://raw.githubusercontent.com/openfido/cli/${OPENFIDO_BRANCH:-main}/src/openfido > /usr/local/bin/openfido ; chmod +x /usr/local/bin/openfido
+curl -sL https://raw.githubusercontent.com/openfido/cli/${OPENFIDO_BRANCH:-main}/src/openfido-server > /usr/local/bin/openfido-server ; chmod +x /usr/local/bin/openfido-server
+curl -sL https://raw.githubusercontent.com/openfido/cli/${OPENFIDO_BRANCH:-main}/src/openfido.py > /usr/local/bin/openfido.py ; chmod +x /usr/local/bin/openfido.py
+curl -sL https://raw.githubusercontent.com/openfido/cli/${OPENFIDO_BRANCH:-main}/src/openfido_util.py > /usr/local/bin/openfido_util.py ; chmod +x /usr/local/bin/openfido_util.py
