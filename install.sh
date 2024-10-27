7e740f168084e4adc74045e7a373663becbe91b0
David P. Chassin <dpchassin@gmail.com>
2024-10-27 07:36:19 -0700

    Update install.sh
    Signed-off-by: David P. Chassin <dpchassin@gmail.com>

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ curl -sL https://raw.githubusercontent.com/${OPENFIDO_PROJECT:-openfido/cli}/${O
 test -x /usr/local/bin/python3 || ln -sf `which python3` /usr/local/bin/python3
 curl -sL https://raw.githubusercontent.com/openfido/cli/main/src/requirements.txt > /tmp/requirements.txt 
 apt-get install python3-pip -y
-python3 -m pip install -r /tmp/requirements.txt $*
+python3 -m pip install -r /tmp/requirements.txt ${OPENFIDO_PIP_OPTIONS}
